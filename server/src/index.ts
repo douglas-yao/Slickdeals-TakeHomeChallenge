d582a57aa9a4fb24d9508b112bd03b121849b676
Douglas Yao <doug.yao@gmail.com>
2023-12-19 14:19:12 -0800

    clean up test code

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,6 @@ import createServer from './utils/createServer';
 
 const app = createServer();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(8080, () => {
   console.log('Server listening on port 8080');
 });
