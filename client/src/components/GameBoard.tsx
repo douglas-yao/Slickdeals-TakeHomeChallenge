6055cbf9752ea592fe98f12152efb56e04eb6924
Douglas Yao <doug.yao@gmail.com>
2023-12-19 14:12:12 -0800

    add instructions

--- client/src/components/GameBoard.tsx
+++ client/src/components/GameBoard.tsx
@@ -14,16 +14,29 @@ export default function GameBoard() {
   if (isLoading) return <span>Loading...</span>;
 
   return (
-    <div className="flex flex-col items-center gap-5">
-      <h1>GameBoard</h1>
-      <button onClick={startNewGame}>New Game</button>
-      <div className="flex gap-10 items-center">
+    <div className="flex flex-col items-center gap-10">
+      <h1>POP!</h1>
+      <div className="flex flex-col items-center">
+        <span>Pop out the lowest number or letter.</span>
+        <span>
+          If the timer expires, or if you pop the wrong tile, the round resets.
+        </span>
+      </div>
+      <button
+        className=" bg-blue-500 text-gray-200 px-4 py-2 rounded-md transition duration-300 hover:bg-blue-400"
+        onClick={startNewGame}
+      >
+        New Game
+      </button>
+      <div className="flex flex-col gap-5 items-center">
         <Row
           currentSolution={currentSolution}
           handleTileClick={handleTileClick}
         />
-        <span>Score: {score}</span>
-        <span>Time Remaining: {timeRemaining}</span>
+        <div className="flex flex-col gap-2 items-center">
+          <span>Score: {score}</span>
+          <span>Time Remaining: {timeRemaining}</span>
+        </div>
       </div>
     </div>
   );
